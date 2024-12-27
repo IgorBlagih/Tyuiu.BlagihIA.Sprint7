74bf875af6aaeb04e993880236be0a99cfcbd134
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-12-27 21:04:00 +0500

    Project.V4

--- Tyuiu.BlagihIA.Sprint7.V4.Test/UnitTest1.cs
+++ Tyuiu.BlagihIA.Sprint7.V4.Test/UnitTest1.cs
@@ -1,3 +1,6 @@
+
+using Tyuiu.BlagihIA.Sprint7.V4.Lib;
+
 namespace Tyuiu.BlagihIA.Sprint7.V4.Test
 {
     [TestClass]
@@ -6,6 +9,21 @@ namespace Tyuiu.BlagihIA.Sprint7.V4.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = Path.Combine("C:", "DataSprint7", "InPutDataFileProjectV4.csv");
+
+            int wait = 71770;
+
+            string[,] tab = Class1.LoadFromFileData(path);
+            int res = 0;
+            for (int i = 1; i < tab.GetLength(0); i++)
+            {
+                for (int j = 3; j <=3; j++)
+                {
+                    res += Convert.ToInt32(tab[i, j]);
+                }
+            }
+
+            Assert.AreEqual(wait, res);
         }
     }
 }
